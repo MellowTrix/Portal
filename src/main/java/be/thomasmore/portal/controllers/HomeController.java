8f585a88a5489aa06bcac1c32d81e02ce106d7d4
MellowTrix <robbe.uytdenhouwen@gmail.com>
2022-04-26 15:26:13 +0200

    Added HomeController

---
+++ src/main/java/be/thomasmore/portal/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package be.thomasmore.portal.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping({"/", '/home'})
+    public String home(Model model) {
+        return "home";
+    }
+}

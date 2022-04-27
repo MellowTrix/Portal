fc1f1d9a8756696a154c3f6bc2d6751a4fac9e4d
MellowTrix <robbe.uytdenhouwen@gmail.com>
2022-04-27 18:46:11 +0200

    added login/logout functionality to home page as admin

---
+++ src/main/java/be/thomasmore/portal/SecurityConfiguration.java
@@ -0,0 +1,18 @@
+package be.thomasmore.portal;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin/**").authenticated()
+                .anyRequest().permitAll()
+                .and().formLogin();
+        http.csrf().ignoringAntMatchers("/h2-console/**")
+                .and().headers().frameOptions().sameOrigin();
+        http.logout().logoutSuccessUrl("/home");
+    }
+}
--- src/main/java/be/thomasmore/portal/controllers/HomeController.java
+++ src/main/java/be/thomasmore/portal/controllers/HomeController.java
@@ -4,11 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.security.Principal;
+
 @Controller
 public class HomeController {
 
     @GetMapping({"/", "/home"})
-    public String home(Model model) {
+    public String home(Model model, Principal principal) {
+        final String loginName = (principal != null) ? principal.getName() : "";
+        model.addAttribute("loginName", loginName);
         return "home";
     }
 

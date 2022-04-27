bb2dea81b2d54b3ce599f5da1528ffdc2c4fa4fc
MellowTrix <robbe.uytdenhouwen@gmail.com>
2022-04-27 21:09:34 +0200

    added user login + basic user registration functionality

---
+++ src/main/java/be/thomasmore/portal/controllers/UserController.java
@@ -0,0 +1,71 @@
+package be.thomasmore.portal.controllers;
+
+import be.thomasmore.portal.models.User;
+import be.thomasmore.portal.repositories.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.naming.AuthenticationException;
+import java.security.Principal;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @GetMapping("/register")
+    public String register(Model model, Principal principal) {
+        if (principal != null) {
+            return "redirect:/home";
+        }
+        User user = new User();
+        model.addAttribute("user", user);
+        return "user/register";
+    }
+
+    @PostMapping("/register")
+    public String register(Model model, @ModelAttribute("user") User user, Principal principal) {
+        if (principal != null) {
+            return "redirect:/home";
+        }
+        if (user.getUsername().equals("") || userRepository.findByUsername(user.getUsername()).isPresent()) {
+            return "redirect:/user/register";
+        }
+        user.setUsername(user.getUsername());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRole("USER");
+        userRepository.save(user);
+        autologin(user.getUsername(), user.getPassword());
+        return "redirect:/home";
+    }
+
+    //TODO Fix autologin
+    private void autologin(String userName, String password) {
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(userName, password);
+
+        Authentication auth = authenticationManager.authenticate(token);
+        logger.info("authentication: " + auth.isAuthenticated());
+        SecurityContext sc = SecurityContextHolder.getContext();
+        sc.setAuthentication(auth);
+    }
+}

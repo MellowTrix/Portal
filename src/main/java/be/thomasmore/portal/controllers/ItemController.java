45752033a14b525a94c4ba3bfdc7a378fdda6e13
BenCras <r0879936@student.thomasmore.be>
2022-04-29 14:04:17 +0200

    webshop starter layout

--- src/main/java/be/thomasmore/portal/controllers/ItemController.java
+++ src/main/java/be/thomasmore/portal/controllers/ItemController.java
@@ -1,16 +1,46 @@
 package be.thomasmore.portal.controllers;
 
+import be.thomasmore.portal.models.Item;
+import be.thomasmore.portal.repositories.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class ItemController {
+    @Autowired
+    private ItemRepository itemRepository;
 
-    @GetMapping("/webshop")
-    public String itemList() {
-
-
+    @GetMapping({"/webshop"})
+    public String itemList(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
         return "webshop";
     }
+
+    @GetMapping({"/itemDetails","/itemDetails/{id}"})
+    public String itemDetails(Model model, @PathVariable(required = false) Integer id) {
+        if (id == null) return "itemDetails";
+
+        Optional<Item> item = itemRepository.findById(id);
+        if (item.isPresent()) {
+            model.addAttribute("item", item.get());
+        }
+
+        return "carDetails";
+    }
+
+//    @GetMapping({"/webshop/filter"})
+//    public String carFilters(Model model, @RequestParam(required = false) Double minPrice, @RequestParam(required = false) Double maxPrice, @RequestParam(required = false) List<String> fuel){
+//        List<Item> items;
+//        items = ItemRepository.findFilter(minPrice, maxPrice, fuel);
+//        model.addAttribute("cars", items);
+//
+//        return "webshop";
+//    }
 
 }
--- src/main/java/be/thomasmore/portal/repositories/ItemRepository.java
+++ src/main/java/be/thomasmore/portal/repositories/ItemRepository.java
@@ -1,10 +1,18 @@
 package be.thomasmore.portal.repositories;
 
 import be.thomasmore.portal.models.Item;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface ItemRepository extends CrudRepository<Item, Integer> {
 
+    @Query("select v from Item v")
+    List<Item> findFilter (@Param("min") Double smoll, @Param("max") Double large, @Param("fuel") List<String> fuel);
 
+    List<Item> findAll();
 
 }
